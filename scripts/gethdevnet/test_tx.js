23cdc945b2a59772267ac06ca330dcf943d7440c
Spencer Solit <sol@seismic.systems>
2024-11-25 10:44:47 -0500

    added tx for seismic-reth

--- scripts/gethdevnet/test_tx.js
+++ scripts/gethdevnet/test_tx.js
@@ -30,7 +30,7 @@ async function sendTransactionInsufficientFunds() {
     }
 }
 
-async function sendTransaction() {
+async function sendTransactionOmni() {
     const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8000");
 
     // Generate random wallets
@@ -59,4 +59,33 @@ async function sendTransaction() {
     }
 }
 
-sendTransaction();
+async function sendTransactionReth() {
+    const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+
+    // Generate random wallets
+    // omni devnet private keys can be found in lib/anvil/accounts.go
+    const privKey1 = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+    const wallet1 = new ethers.Wallet(privKey1, provider);
+    const wallet2 = Wallet.createRandom();
+
+    const recipientAddress = wallet2.address;
+
+    const tx = {
+        to: recipientAddress,
+        value: ethers.parseEther("0.01"),
+        gasLimit: 21000,
+        gasPrice: ethers.parseUnits("10", "gwei"),
+    };
+
+    try {
+        const transactionResponse = await wallet1.sendTransaction(tx);
+        console.log("Transaction sent! Hash:", transactionResponse.hash);
+
+        const receipt = await transactionResponse.wait();
+        console.log("Transaction mined! Receipt:", receipt);
+    } catch (error) {
+        console.error("Error sending transaction:", error);
+    }
+}
+
+sendTransactionReth();
